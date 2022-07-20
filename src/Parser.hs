89b902399470d8a11e7dcd2c114c9847db03bcad
Bhavik Mehta <bhavikmehta8@gmail.com>
2022-07-20 20:36:08 +0100

    Add the parser properly

---
+++ src/Parser.hs
@@ -0,0 +1,121 @@
+{-|
+A parser for a basic Lisp-like syntax into an internal expression.
+-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Parser where
+
+import Control.Applicative hiding (many, some)
+import Control.Monad.State
+import Data.Foldable
+import Data.Functor.Identity
+import Data.HashMap.Strict (HashMap)
+import Data.List.NonEmpty
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Control.Monad.Combinators.NonEmpty as NE
+import qualified Data.HashMap.Strict as M
+
+import Lib2
+
+-- Uses megaparsec with a custom state to keep track of local bindings
+-- See https://markkarpov.com/tutorial/megaparsec.html for a tutorial on
+-- megaparsec. In the future, might want to change the monad from StateT to
+-- something else which runs mutably, but should test to see. STRef or IORef
+-- might be more efficient, but also might give overheads (and IORef restricts
+-- how it can be used).
+type Parser =
+    ParsecT Void String
+    (StateT (HashMap String InternalName, Int) Identity)
+
+exampleStrings :: [String]
+exampleStrings =
+  [ "succ"
+      -- test constants
+  , "(succ zero)"
+      -- test application + constants
+  , "(not ((eq (succ zero)) zero))"
+      -- test more complex application
+  , "(not (eq (succ zero) zero))"
+      -- test n-ary application
+  , "forall x, (not (eq (succ zero) zero))"
+      -- test binders
+  , "forall x, (not (eq (succ x) zero))"
+      -- test binding (peano one)
+  , "forall x, forall y, (implies (eq (succ x) (succ y)) (eq x y))"
+      -- test multiple binding (peano two)
+  ]
+
+-- | The general expression parser.
+parseExpr :: Parser Expr
+parseExpr = parseApps <|> parseForall <|> parseFree <|> parseCon
+
+-- | Parse a multi-application
+parseApps :: Parser Expr
+parseApps = do
+  f :| xs <- char '(' *> NE.sepEndBy1 parseExpr hspace <* char ')'
+  return $ apps f xs
+
+-- | Register a new identifier, and return its internal name
+registerIdent :: MonadState (HashMap String InternalName, Int) m
+              => String -> m InternalName
+registerIdent s = do
+  (m, i) <- get
+  put (M.insert s [("parser", i)] m, i + 1)
+  return [("parser", i)]
+
+-- | Parse a forall expression
+parseForall :: Parser Expr
+parseForall = do
+  _ <- string "forall "
+  i <- parseIdent
+  nm <- registerIdent i
+    -- ^ it's important that this runs before the e <- parseExpr
+  _ <- string ", "
+  e <- parseExpr
+  return (forall (Just (ExternalName i)) nm e)
+
+-- | Parse a free variable (must be known from the context)
+parseFree :: Parser Expr
+parseFree = try $ do
+  i <- parseIdent
+  (m, _) <- get
+  case m M.!? i of
+    Just n -> return $ Free n
+    Nothing -> fail "unrecognised identifier"
+      -- the name used wasn't recognised
+      -- not sure if this error can even happen?
+
+-- | Parse a constant
+parseCon :: Parser Expr
+parseCon = try (Con <$> some letterChar)
+  -- this try might be unnecessary but if the syntax changes it could become
+  -- crucial
+
+-- | Parse an identifier (doesn't register it!)
+parseIdent :: Parser String
+parseIdent = try (some letterChar)
+
+-- | Run the parser in the IO monad - mostly for testing.  This assumes an
+-- empty context - if we already know for example that "x" means something
+-- and has an existing internal name, then this doesn't apply.
+parseTestT :: Show a => Parser a -> String -> IO ()
+parseTestT p inp =
+  case evalState (runParserT p "example" inp) (M.empty, 0) of
+      -- change the (M.empty, 0) to change the context
+    Left e -> putStr (errorBundlePretty e)
+    Right x -> print x
+
+-- | Run the parser, returning Just _ on success, and `Nothing` on failure.
+-- This assumes an empty context - if we already know for example that "x"
+-- means something and has an existing internal name, then this doesn't apply.
+parseSimple :: Parser Expr -> String -> Maybe Expr
+parseSimple p inp =
+  case evalState (runParserT p "example" inp) (M.empty, 0) of
+    Left _ -> Nothing
+    Right x -> Just x
+
+-- | Run the examples.
+runExamples :: IO ()
+runExamples = for_ exampleStrings $ parseTestT (parseExpr <* eof)
